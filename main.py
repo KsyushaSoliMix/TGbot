7c54a982e4e9890edbdd50c99070257bc4a6d354
StariyMicrozavr <artjomilin03@gmail.com>
2022-04-24 02:03:59 +0300

    Add my_languages func

--- main.py
+++ main.py
@@ -9,20 +9,22 @@ bot = telebot.TeleBot(token)
 
 # Переменные для команд
 
-languages = "🇬🇧 English", "🇩🇪 Deutsch", "🇷🇺 Русский"
+languages = ["🇬🇧 English", "🇩🇪 Deutsch", "🇷🇺 Русский"]
 
-help_line = "/newLanguage - добавляет новый язык.\n" \
-            "/study *язык* - команда для продолжения изучения языка."
-
+help_line = "/new_language - добавляет новый язык.\n" \
+            "/my_languages - показывает уже изучаемые вами языки\n" \
+            "/study *язык* - команда для продолжения изучения языка.\n"
 
 # ----------------------------------------------------------
+
+my_languages = []
 
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, "Привет " + message.from_user.first_name  +"!\n" \
-             "Study language bot поможет тебе в изучении разных иностранных языков.\n" \
-             "Введи /help для того, чтобы посмотреть функционал.")
+    bot.send_message(message.chat.id, "Привет " + message.from_user.first_name + "!\n" \
+                                                                                 "Study language bot поможет тебе в изучении разных иностранных языков.\n" \
+                                                                                 "Введи /help для того, чтобы посмотреть функционал.")
 
 
 @bot.message_handler(commands=['help'])
@@ -31,7 +33,7 @@ def help_message(message):
 
 
 # Выбор языков:
-@bot.message_handler(commands=['newLanguage'])
+@bot.message_handler(commands=['new_language'])
 def new_language_message(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     for i in range(0, len(languages)):
@@ -40,14 +42,33 @@ def new_language_message(message):
     bot.send_message(message.chat.id, "Выберите язык", reply_markup=markup)
 
 
+@bot.message_handler(commands=['my_languages'])
+def get_my_languages_message(message):
+    help_string = ""
+    for i in range(0, len(my_languages)):
+        help_string += my_languages[i] + "\n"
+    if help_string == "":
+        bot.send_message(message.chat.id, "Вы еще не выбрали ни одного языка :(")
+    else:
+        bot.send_message(message.chat.id, help_string)
+
+
 @bot.message_handler(content_types=['text'])
 def languages_handling(message):
-    if (message.text == "🇬🇧 English"):
-        bot.send_message(message.chat.id, 'Now you are a englishman')
-    elif (message.text == "🇩🇪 Deutsch"):
-        bot.send_message(message.chat.id, 'Jetzt du bist Deutsch Person')
-    elif (message.text == "🇷🇺 Русский"):
+    a = telebot.types.ReplyKeyboardRemove()
+    if message.text == "🇬🇧 English":
+        str = "🇬🇧 English"
+        languages.remove(str)
+        bot.send_message(message.chat.id, 'Now you are a englishman', reply_markup=a)
+        my_languages.append(str)
+    elif message.text == "🇩🇪 Deutsch":
+        str = "🇩🇪 Deutsch"
+        languages.remove(str)
+        bot.send_message(message.chat.id, 'Jetzt du bist Deutsch Person', reply_markup=a)
+        my_languages.append(str)
+    elif message.text == "🇷🇺 Русский":
         bot.send_message(message.chat.id, 'Уйди отсюда, пидор грязный')
+
 
 # ----------------------------------------------------------
 bot.polling(none_stop=True)
