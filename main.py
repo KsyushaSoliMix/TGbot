0e2cb6e3c2fbcb10b779e12430aad0712b2ee762
StariyMicrozavr <artjomilin03@gmail.com>
2022-04-23 19:07:11 +0300

    Add newLanguage buttons

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
+# Библиотеки
 import telebot
+from telebot import types
 
+# Токен и создание бота
 token = '5305392177:AAGRLxjBJ43TgZSo7qC8XoXRJ75bKCzh7Fk'
 bot = telebot.TeleBot(token)
+# -------------------------------------------------------
 
-
+# Переменные для команд
 start_line = "Привет ✌ \n" \
              "Study language bot поможет тебе в изучении разных иностранных языков.\n" \
              "Введи /help для того, чтобы посмотреть функционал."
 
-languages = "English\n"
+languages = "English", "Deutsch", "Русский"
 
-help_line = "/newLanguage *ваш язык для изучения* - добавляет новый язык. Доступные языки:\n" + languages +\
+help_line = "/newLanguage - добавляет новый язык.\n" \
             "/study *язык* - команда для продолжения изучения языка."
+
+
+# ----------------------------------------------------------
 
 
 @bot.message_handler(commands=['start'])
@@ -26,14 +33,19 @@ def help_message(message):
 
 @bot.message_handler(commands=['newLanguage'])
 def newLanguage_message(message):
-    bot.send_message(message.chat.id, help_line)
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    for i in range(0, len(languages)):
+        item = types.KeyboardButton(languages[i])
+        markup.add(item)
+    bot.send_message(message.chat.id, "Выберите язык", reply_markup=markup)
 
 
 @bot.message_handler(content_types=['text'])
 def hello(message):
     if message.text == "Hello":
-      bot.send_message(message.chat.id, message.text)
+        bot.send_message(message.chat.id, message.text)
     elif message.text == "Привет":
-      bot.send_message(message.chat.id, "Ненавижу глупых русских!!!")
+        bot.send_message(message.chat.id, "Ненавижу глупых русских!!!")
+
 
 bot.polling(none_stop=True)
