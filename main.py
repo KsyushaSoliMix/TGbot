f241445a3570b400511301d3698bb33f2815380b
StariyMicrozavr <artjomilin03@gmail.com>
2022-04-24 15:32:39 +0300

    Добавил сохранение языков в базу данных для каждого пользователя

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 # Библиотеки
+import re
+
 import telebot
 from telebot import types
 import sqllite_db
@@ -19,49 +21,76 @@ help_line = "/new_language - добавляет новый язык.\n" \
 
 create_users = """
 INSERT INTO users 
-   (user_id, name, languages)
-VALUES (?, ?, ?);"""
+   (user_id, languages)
+VALUES (?, ?);"""
+
+
+
 # ----------------------------------------------------------
-
-my_languages = []
-
 
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
     # Создаем нового юзера и добавляем его в базу данных, с проверкой на то, был ли он до этого зарегестрирован
     info = sqllite_db.cursor.execute('SELECT * FROM users WHERE user_id=?', (message.from_user.id,))
+    info.fetchone()
     if info.fetchone() is None:
         bot.send_message(message.chat.id, "Привет " + message.from_user.first_name + "!\n" \
                                                                                      "Study language bot поможет тебе в изучении разных иностранных языков.\n" \
                                                                                      "Введи /help для того, чтобы посмотреть функционал.")
-        person_data = (message.from_user.id, message.from_user.first_name, "")
+        person_data = (message.from_user.id, "")
         sqllite_db.cursor.execute(create_users, person_data)
         sqllite_db.connection.commit()
     else:
-        bot.send_message(message.chat.id, "Привет " + message.from_user.first_name + "! Готов продолжить обучение?\n Вводи команду /study и поехали!!!")
+        bot.send_message(message.chat.id,
+                         "Привет " + message.from_user.first_name + "! Готов продолжить обучение?\nВводи команду /study и поехали!!!")
+
 
 @bot.message_handler(commands=['help'])
 def help_message(message):
     bot.send_message(message.chat.id, help_line)
 
 
+# Функции для получения и сохранения языков:
+def get_languages(user_id):
+    info = sqllite_db.cursor.execute('SELECT * FROM users WHERE user_id=?', (user_id,))
+    record = info.fetchone()
+    return record[1]
+
+
+def set_language(user_id, language):
+    help_language = get_languages(user_id) + language + "&"
+    sqllite_db.cursor.execute('UPDATE users SET languages = ? WHERE user_id = ?', (help_language, user_id))
+    sqllite_db.connection.commit()
+    print(get_languages(user_id))
+
+
 # Выбор языков:
 @bot.message_handler(commands=['new_language'])
 def new_language_message(message):
+    users_languages = get_languages(message.from_user.id)
+    split_user_languages = re.split("&", users_languages)
+    other_languages = languages
+    print(split_user_languages)
+    for i in range(0, len(split_user_languages)):
+        if other_languages.__contains__(split_user_languages[i]):
+            other_languages.remove(split_user_languages[i])
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    for i in range(0, len(languages)):
-        item = types.KeyboardButton(languages[i])
+    for i in range(0, len(other_languages)):
+        item = types.KeyboardButton(other_languages[i])
         markup.add(item)
     bot.send_message(message.chat.id, "Выберите язык", reply_markup=markup)
 
 
 @bot.message_handler(commands=['my_languages'])
 def get_my_languages_message(message):
+    users_languages = get_languages(message.from_user.id)
+    split_user_languages = re.split(" ", users_languages)
     help_string = ""
-    for i in range(0, len(my_languages)):
-        help_string += my_languages[i] + "\n"
-    if help_string == "":
+    for i in range(0, len(split_user_languages)):
+        help_string += split_user_languages[i] + "\n"
+    print(help_string)
+    if help_string == "\n":
         bot.send_message(message.chat.id, "Вы еще не выбрали ни одного языка :(")
     else:
         bot.send_message(message.chat.id, help_string)
@@ -72,14 +101,12 @@ def languages_handling(message):
     a = telebot.types.ReplyKeyboardRemove()
     if message.text == "🇬🇧 English":
         str = "🇬🇧 English"
-        languages.remove(str)
         bot.send_message(message.chat.id, 'Now you are a englishman', reply_markup=a)
-        my_languages.append(str)
+        set_language(message.from_user.id, str)
     elif message.text == "🇩🇪 Deutsch":
         str = "🇩🇪 Deutsch"
-        languages.remove(str)
         bot.send_message(message.chat.id, 'Jetzt du bist Deutsch Person', reply_markup=a)
-        my_languages.append(str)
+        set_language(message.from_user.id, str)
     elif message.text == "🇷🇺 Русский":
         bot.send_message(message.chat.id, 'Уйди отсюда, пидор грязный')
 
